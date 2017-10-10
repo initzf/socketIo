71c4d40e14c16ecaa391fac01017e34ef8c13bef
fang.zhang <initzf@126.com>
2017-10-11 00:24:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhangfang on 2017/10/10.
+ */
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3000);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
